75796ccc4f323398492b664573c6c21f8e0b1708
Volodymyr Isyk <volodka.isyk.95@gmail.com>
2024-03-09 22:15:04 +0200

    fix: added main in package.json

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ const path = require('path')
 const filePath = path.join('db.json')
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
+
 const router = jsonServer.router(db)
 
 const middlewares = jsonServer.defaults();
